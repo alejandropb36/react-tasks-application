bd399abb15054dfadfc19ba61b37ce8f1c6a80d2
Alejandro Ponce <alexponce36@hotmail.com>
2022-11-05 10:58:25 -0600

    feat: lista de tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import TaskList from './TaskList'
+
 function App() {
   return(
     <>
-      <h1>Hola mundo</h1>
+    <TaskList />
     </>
   );
 }
---
+++ src/TaskList.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { tasks as data } from "./tasks";
+
+function TaskList() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  if (tasks.length === 0) {
+    return <h2>No hay tareas para mostrar</h2>;
+  }
+
+  return (
+    <div>
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <h1>{task.title}</h1>
+          <p>{task.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;
