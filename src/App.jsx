868e49b11a738cda5b4c13790fcc9902f6dd3f41
Alejandro Ponce <alexponce36@hotmail.com>
2022-11-08 21:23:49 -0600

    feat: delete tasks

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,23 @@ function App() {
 
   function createTask(task) {
     const newTask = {
-      id: tasks.length,
+      id: tasks[tasks.length - 1].id + 1,
       title: task.title,
       description: task.description
     }
     setTasks([...tasks, newTask]);
   }
 
+  function deleteTask(id) {
+    console.log(`Elimando tarea id: ${id} ...`);
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+
 
   return(
     <>
     <TaskForm createTask={createTask}/>
-    <TaskList tasks={tasks}/>
+    <TaskList tasks={tasks} deleteTask={deleteTask}/>
     </>
   );
 }
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,8 +1,13 @@
-function TaskCard({task}) {
+function TaskCard({task, deleteTask}) {
+
+  function mostrarAlerta() {
+    alert("Elimando...");
+  }
   return (
     <div>
         <h1>{task.title}</h1>
         <p>{task.description}</p>
+        <button onClick={() => deleteTask(task.id)}>Elimar tarea</button>
     </div>
   )
 }
